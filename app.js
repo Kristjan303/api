ddddf212eabdcec7c67a6b999ef7856f1948af43
Kristjan-Even <kristjan.puusepp@voco.ee>
2022-10-25 13:24:50 +0300

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 document.addEventListener(`DOMContentLoaded`, cityWeather)
 
 function weatherDataFetch(city){
-    var key = "a19f0c57c30323d6fca1351aa55b22df";
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid${key}`)
+    var key = "5919524c963e97110290a856859fbdec";
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
         .then(function (resp) {
             return resp.json()
         })
         .then(function (data) {
             console.log(data);
+            drawWeather(data);
         })
         .catch(function () {
 
@@ -16,4 +17,13 @@ function weatherDataFetch(city){
 
 function cityWeather(e) {
     weatherDataFetch(`Tallinn`);
+}
+
+function drawWeather(data) {
+    var celcius = Math.round(parseFloat(data.main.temp) -273.15);
+    var description = data.weather[0].description;
+
+    document.querySelector(`#description`).innerHTML = description;
+    document.querySelector(`#temp`).innerHTML = celcius + `&deg;`;
+    document.querySelector(`#location`).innerHTML = data.name;
 }
